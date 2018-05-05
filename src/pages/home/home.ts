c4ea349fbf612c2c98acbf011355eb3c91eb6f31
Abdul Basit <abasitm.1993@gmail.com>
2018-05-05 21:48:46 +0500

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, ModalController } from 'ionic-angular';
+import { AddtodoPage } from '../addtodo/addtodo';
+import { DataProvider } from '../../providers/data/data';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  todos = [];
+
+  constructor(
+    public navCtrl: NavController,
+    private alert: AlertController,
+    private modal: ModalController,
+    private dataProvider: DataProvider
+  ) {
+    this.dataProvider.get().then(todos => {
+      this.todos = todos;
+    });
+  }
+  
+
+  addTodo() {
+    let addTodoPage = this.modal.create(AddtodoPage);
+
+    addTodoPage.onDidDismiss(todo => {
+      if (todo && todo.description) {
+        this.todos.push(todo);
+
+        this.dataProvider.save(this.todos);
+      }
+    });
+
+    addTodoPage.present();
+  }
+
+  deleteTodo(todo) {
+    let index = this.todos.indexOf(todo, 0);
+
+    if (index > -1) {
+      this.alert.create({
+        title: "Confirm",
+        message: "Are you sure you want to delete?",
+        buttons: [
+          { text: "Cancel" },
+          {
+            text: "Delete",
+            handler: () => {
+              this.todos.splice(index, 1);
+
+              this.dataProvider.save(this.todos);
+            }
+          }
+        ]
+      }).present();
+    }
+
+  }
+
+  about() {
+    this.alert.create({
+      title: "About TodoApp",
+      message: "Todo List App v1.0 by Abdul Basit Mehtab. Best for outlining your everyday tasks with ease.",
+      buttons: ['Okay']
+    }).present();
+  }
+
+}
